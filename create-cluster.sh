17026020d7c17bc20504142d58034315778c329c
Joep van Diessen <joepvandiessen@gmail.com>
2022-05-20 23:44:23 +0200

    Update create-cluster.sh

--- create-cluster.sh
+++ create-cluster.sh
@@ -49,6 +49,6 @@ kubectl taint node --all node-role.kubernetes.io/master-
 kubectl taint node --all node-role.kubernetes.io/control-plane-
 
 # Install Cilium
-# wget https://github.com/cilium/cilium-cli/releases/latest/download/cilium-linux-amd64.tar.gz
-# sudo tar xzvfC cilium-linux-amd64.tar.gz /usr/local/bin
-# cilium install
+wget https://github.com/cilium/cilium-cli/releases/latest/download/cilium-linux-amd64.tar.gz
+sudo tar xzvfC cilium-linux-amd64.tar.gz /usr/local/bin
+cilium install
