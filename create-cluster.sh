94cf1f4002368fd6cec47811bb57c4df9c90e033
Joep van Diessen <joepvandiessen@gmail.com>
2022-06-16 11:41:21 +0200

    Update create-cluster.sh

--- create-cluster.sh
+++ create-cluster.sh
@@ -19,8 +19,8 @@ sudo systemctl restart docker
 # Install cri-dockerd
 VER=$(curl -s https://api.github.com/repos/Mirantis/cri-dockerd/releases/latest|grep tag_name | cut -d '"' -f 4 | sed 's/v//g')
 
-wget https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dockerd-${VER}.amd64.tgz
-tar xvf cri-dockerd-${VER}.amd64.tgz
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.2.2/cri-dockerd-0.2.2.amd64.tgz
+tar xvf cri-dockerd-0.2.2.amd64.tgz
 sudo mv cri-dockerd /usr/local/bin/
 sudo wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/50c048cb54e52cd9058f044671e309e9fbda82e4/packaging/systemd/cri-docker.service
 sudo wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/50c048cb54e52cd9058f044671e309e9fbda82e4/packaging/systemd/cri-docker.socket
